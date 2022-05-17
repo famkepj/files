fefb171268d53a76df20f995933352d42d45bcd3
Famke Philips <Famkepj@gmail.com>
2022-05-17 19:18:38 +0200

    add repostery

---
+++ main.py
@@ -0,0 +1,48 @@
+__winc_id__ = "ae539110d03e49ea8738fd413ac44ba8"
+__human_name__ = "files"
+
+
+import os
+import shutil
+from zipfile import ZipFile
+
+import os
+import shutil
+from zipfile import ZipFile
+
+def main():
+    clean_cache() 
+    cache_zip('files/data.zip', 'files/cache' )
+    (cached_files()) 
+    print(find_password(cached_files()))
+
+def clean_cache ():
+    if not os.path.exists('files/cache'):
+        os.makedirs('files/cache')
+    else: 
+        shutil.rmtree('files/cache')
+        os.makedirs('files/cache')
+    return dir
+
+def cache_zip(zip_file_path, cache_dir_path):
+    with ZipFile(zip_file_path, 'r') as zipObj:
+        zipObj.extractall(cache_dir_path)
+    return zipObj
+
+def cached_files():
+    path= r'C:\Users\famke\OneDrive\Documenten\Programmeren\Winc-Academy (back-end)\files\cache'
+    list_cached_files = [os.path.join(path, file) for file in os.listdir(path)]
+    return list_cached_files
+
+def find_password(list_cached_files):
+    for file in list_cached_files:
+        open_file = open(file,'r')
+        text = open_file.readlines()
+        if 'password' in str(text):
+            for string in text:
+                if 'password' in string:
+                    password = (string[string.find(' ')+1:-1])
+    return password
+
+if __name__ == '__main__':
+    main()
